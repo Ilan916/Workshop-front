6a03ad782ff16af9cc0b0d346119aa13ca33a9b2
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2024-01-29 22:37:18 +0100

    refonte font

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+<<<<<<< Updated upstream
 
 import LandingPage from './components/landingPage';
 
+=======
+import { Landing, Search } from './pages';
+import { BarCodeScanner } from './components';
+import Header from './components/header';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <Router>
+      <Header navigation={[
+        { name: 'Home', href: '/' },
+        { name: 'GPS', href: '/gps' },
+        { name: 'Barcode Scanner', href: '/bar' },
+      ]} />
     <Switch>
       <Route path="/" exact component={LandingPage} />
       <Route path="/scanpage" component={LandingPage} />
