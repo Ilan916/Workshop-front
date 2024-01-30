19a3a9488e5ff05719d63c5791b30815daed6400
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-01-30 09:25:14 +0100

    add
    add

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-<Router>
-  
-      <Header navigation={[
-        { name: 'Home', href: '/' },
-        { name: 'GPS', href: '/gps' },
-        { name: 'Barcode Scanner', href: '/bar' },
-      ]} />
+    <Router>
+      <Header
+        navigation={[
+          { name: "Home", href: "/" },
+          { name: "GPS", href: "/gps" },
+          { name: "Barcode Scanner", href: "/bar" },
+          { name: "News", href: "/news" },
+        ]}
+      />
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/gps" component={Search} />
         <Route path="/bar" component={BarCodeScanner} />
+        <Route path="/news" component={News} />
       </Switch>
-      <Footer/>
+      <Footer />
     </Router>
-
   );
 }
 
