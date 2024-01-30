69a88410967c1828b882afbea610447a4326bb5f
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-01-30 10:46:58 +0100

    add cube
    add cube

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -1,23 +1,24 @@
-// Front-end code (BarcodeScanner.js)
 import React, { useState, useEffect } from "react";
 import Quagga from "quagga";
 import axios from "axios";
+import "aframe"; // Importez A-Frame
 
 const BarcodeScanner = () => {
   const [barcode, setBarcode] = useState("");
   const [productInfo, setProductInfo] = useState(null);
+  const [scanSuccess, setScanSuccess] = useState(false);
 
   useEffect(() => {
     const handleDetected = (data) => {
       setBarcode(data.codeResult.code);
-      Quagga.offDetected(); // Stop further scanning after the first successful scan
-      // Send the scanned barcode to your back-end
+      Quagga.offDetected();
       axios
         .post("http://localhost:3000/product/search-by-barcode", {
           barcode: data.codeResult.code,
         })
         .then((response) => {
           setProductInfo(response.data);
+          setScanSuccess(true);
         })
         .catch((error) => {
           console.error(
@@ -62,23 +63,45 @@ const BarcodeScanner = () => {
   }, []);
 
   return (
-    <div className="scanner" style={{ color: "white" }}>
+    <div>
       <div
         id="scanner-container"
         style={{ width: "640px", height: "480px", backgroundColor: "black" }}
       />
-      {barcode && <p>Code-Barres: {barcode}</p>}
+
+      {barcode && <p style={{ color: "white" }}>Code-Barres: {barcode}</p>}
+
       {productInfo && (
         <div>
-          <h3>Informations sur le Produit</h3>
-          <p>Nom du Produit: {productInfo.name}</p>
-          <p>Description: {productInfo.description}</p>
-          <p>Catégorie: {productInfo.category}</p>
-          {/* Include more product information here */}
+          <h3 style={{ color: "white" }}>Informations sur le Produit</h3>
+          <p style={{ color: "white" }}>Nom du Produit: {productInfo.name}</p>
+          <p style={{ color: "white" }}>
+            Description: {productInfo.description}
+          </p>
+          <p style={{ color: "white" }}>Catégorie: {productInfo.category}</p>
         </div>
       )}
+
       {!productInfo && barcode && (
-        <p>Aucune information trouvée pour ce produit.</p>
+        <p style={{ color: "white" }}>
+          Aucune information trouvée pour ce produit.
+        </p>
+      )}
+
+      {scanSuccess && (
+        <a-scene>
+          <a-marker-camera preset="hiro" />
+          <a-box position="0 0 -5" color="red" scale="2 2 2">
+            <a-text
+              value={`Nom du Produit: ${productInfo.name}\nCatégorie: ${productInfo.category}`}
+              position="-0.7 0.3 0.5"
+              color="black"
+              align="left"
+              width="1.0"
+              wrap-count="20"
+            ></a-text>
+          </a-box>
+        </a-scene>
       )}
     </div>
   );
